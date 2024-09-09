b401d21f9fa5f2d337936305018a64935dfbbc96
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-09-09 17:49:52 +0300

    cell hover

--- js/index.js
+++ js/index.js
@@ -1,18 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
   const table = document.getElementById('table');
-  const cells = table.getElementsByTagName('td');
+  const cells = Array.from(table.getElementsByTagName('td'));
 
   function handleClick(evt) {
     const target = evt.target;
     if (target.tagName === 'TD') {
       const symbol = target.textContent;
-      for (let i = 0; i < cells.length; i++) {
-        if (cells[i].textContent === symbol) {
-          cells[i].textContent = '';
+      cells.forEach((cell) => {
+        if (cell.textContent === symbol) {
+          cell.textContent = '';
         }
-      }
+      });
+    }
+  }
+
+  function handleMouseOver(evt) {
+    const target = evt.target;
+    if (target.tagName === 'TD') {
+      const symbol = target.textContent;
+      cells.forEach((cell) => {
+        if (cell.textContent === symbol) {
+          cell.classList.add('background');
+        }
+      });
+    }
+  }
+
+  function handleMouseOut(evt) {
+    if (evt.target.tagName === 'TD') {
+      const symbol = evt.target.textContent;
+      cells.forEach((cell) => {
+        if (cell.textContent === symbol) {
+          cell.classList.remove('background');
+        }
+      });
     }
   }
 
   table.addEventListener('click', handleClick);
+  table.addEventListener('mouseover', handleMouseOver);
+  table.addEventListener('mouseout', handleMouseOut);
 });
